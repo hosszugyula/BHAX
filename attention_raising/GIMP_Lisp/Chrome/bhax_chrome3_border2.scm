491265fcaa157b33692cdec6bcb558c6bfd032c6
hosszugyula <hosszu87@gmail.com>
2020-05-01 12:41:25 +0200

    Chaitin kész

--- attention_raising/GIMP_Lisp/Chrome/bhax_chrome3_border2.scm
+++ attention_raising/GIMP_Lisp/Chrome/bhax_chrome3_border2.scm
@@ -147,7 +147,7 @@
     "Copyright 2019, Norbert Bátfai"
     "January 19, 2019"
     ""
-    SF-STRING       "Text"      "Bátf41 Haxor"
+    SF-STRING       "Text"      "HosszúGyulaVideos"
     SF-FONT         "Font"      "Sans"
     SF-ADJUSTMENT   "Font size" '(160 1 1000 1 10 0 1)
     SF-VALUE        "Width"     "1920"
